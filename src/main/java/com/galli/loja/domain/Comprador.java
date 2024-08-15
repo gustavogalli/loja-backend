8a1611698145a71e453a2c43563d109bdd49a4b8
gustavogalli <gustavosgalli@gmail.com>
2024-08-15 18:35:01 -0300

    Adding Comprador and Vendedor classes

--- src/main/java/com/galli/loja/domain/Comprador.java
+++ src/main/java/com/galli/loja/domain/Comprador.java
@@ -1,4 +1,30 @@
-package com.galli.loja.domain.domain;
+package com.galli.loja.domain;
 
-public class Comprador {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comprador extends Usuario{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "comprador_id")
+    public List<Produto> carrinho = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "comprador_id")
+    public List<Produto> produtosComprados = new ArrayList<>();
+
 }
--- src/main/java/com/galli/loja/domain/Vendedor.java
+++ src/main/java/com/galli/loja/domain/Vendedor.java
@@ -1,4 +1,19 @@
 package com.galli.loja.domain;
 
-public class Vendedor {
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Vendedor extends Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "comprador_id")
+    public List<Produto> estoque = new ArrayList<>();
+
+
 }
