1f787a0e3d57a06c649bdc40fc9575a198bc4308
gustavogalli <gustavosgalli@gmail.com>
2024-09-03 09:51:30 -0300

    Adding data.sql to create admin user

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users values (1, '$2a$10$NWM0kcUU1csGRPXmz/LpM.z6XCRL2fANh1OQhonCsZC2fskKk5T/q', 'admin');
+INSERT INTO roles values (1, 'ROLE_ADMIN'), (2, 'ROLE_USER');
+INSERT INTO users_roles values (1, 1);
