eb02c6f2f835e4a4dd65c5aac0ee5ac2074c0ab8
gustavogalli <gustavosgalli@gmail.com>
2024-09-03 10:20:36 -0300

    Updating data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
-INSERT INTO users values (1, '$2a$10$NWM0kcUU1csGRPXmz/LpM.z6XCRL2fANh1OQhonCsZC2fskKk5T/q', 'admin');
-INSERT INTO roles values (1, 'ROLE_ADMIN'), (2, 'ROLE_USER');
-INSERT INTO users_roles values (1, 1);
+INSERT INTO users (user_id, password, username)
+SELECT 1, '$2a$10$NWM0kcUU1csGRPXmz/LpM.z6XCRL2fANh1OQhonCsZC2fskKk5T/q', 'admin'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE user_id = 1);
+
+INSERT INTO roles (role_id, rolename)
+SELECT 1, 'ROLE_ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_id = 1);
+
+INSERT INTO roles (role_id, rolename)
+SELECT 2, 'ROLE_USER'
+WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_id = 2);
+
+INSERT INTO users_roles (user_id, role_id)
+SELECT 1, 1
+WHERE NOT EXISTS (SELECT 1 FROM users_roles WHERE user_id = 1 AND role_id = 1);
